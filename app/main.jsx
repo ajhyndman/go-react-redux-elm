000c2a7f02446fc8b886f3f69edbdfb5ddcaeac5
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-15 22:26:25 +1000

    fixup: unused code

--- app/main.jsx
+++ app/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import { range } from './utils';
 import Board from './board';
 import { ContainerView } from './components.jsx!';
 
-// [...range(0, 10, 2)].forEach((value) => console.log(value));
 
-var board = new Board(19);
+let board = new Board(19);
 
 ReactDOM.render(
     <ContainerView board={board} />,
