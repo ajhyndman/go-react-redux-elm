acfe7ad641d18afaa5c449abae06d52b5d95f34e
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-22 02:53:59 +1000

    Add simple reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,35 @@
 import ReduxImmutable from 'redux-immutable';
 
+import constants from './utils/constants';
 
-const reducers = ReduxImmutable.combineReducers({});
+
+/**
+ * The store for this go board game is just one giant 2D array.
+ *
+ * Each entry in the grid represents the state of that position on the board.
+ * States may be: EMPTY, BLACK, or WHITE.
+ *
+ * e.g. (for a 3x3 board)
+ * [
+ *     [EMPTY, EMPTY, EMPTY],
+ *     [EMPTY, BLACK, EMPTY],
+ *     [WHITE, EMPTY, EMPTY],
+ * ]
+ *
+ */
 
 
-export default reducers;
+const reducer = function (state, action) {
+    switch (action.type) {
+        case constants.actionTypes.PLACE_STONE:
+            return state;
+        case constants.actionTypes.PASS:
+            return state;
+        case default:
+            return state;
+    }
+
+};
+
+
+export default reducer;
