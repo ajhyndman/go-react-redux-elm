9c1f310f3a0b38fb10548c64e2441740e9b4284f
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-22 01:47:49 +1000

    Rewrite board state as a Store

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Redux from 'redux';
+import ReactRedux from 'react-redux';
 
-import Board from './board';
-import { ContainerView } from './components.jsx!';
+import reducers from './reducers';
+import range from './utils/range';
+import ContainerView from './components.jsx!';
 
 
-let board = new Board(19);
+// Globals
+const SIZE = 19;
+const STATE = {
+    empty: 0,
+    black: 1,
+    white: 2,
+};
 
+// Tracks only the current board state
+const board = Redux.createStore(
+    reducers,
+    [...range(0, SIZE)].map(() => (
+        [...range(0, SIZE)].map(() => STATE.empty)
+    ))
+);
+
+// Renders the game board
 ReactDOM.render(
     <ContainerView board={board} />,
     document.getElementById('app')
