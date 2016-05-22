92f9268c40392e6c6899b9ba30ccd2baa9640f45
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-23 01:31:27 +1000

    Implement and connect PassButton

---
+++ src/components/PassButton.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import 'materialize-css';
+
+import C from '../utils/constants';
+import store from '../store';
+
+
+const ConnectedPassButton = (props) => (
+    <button
+        className="waves-effect waves-light btn blue-grey darken-4"
+        onClick={props.onClick}
+    >
+        Pass
+    </button>
+);
+
+ConnectedPassButton.propTypes = {
+    onClick: React.PropTypes.func,
+};
+
+const PassButton = React.createClass({
+    componentDidMount: function () {
+        this.unsubscribe = store.subscribe(function () {
+            this.forceUpdate();
+        }.bind(this));
+    },
+    componentWillUnmount: function () {
+        this.unsubscribe();
+    },
+    onClick: function () {
+        store.dispatch({ type: C.actionTypes.PASS });
+    },
+    render: function () {
+        const state = store.getState();
+        return <ConnectedPassButton onClick={this.onClick} />;
+    },
+});
+
+
+export default PassButton;
