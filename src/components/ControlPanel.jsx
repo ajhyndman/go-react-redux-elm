bfb1a7b367c42129c3ae54d15c2deb6f8c10f4bf
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-23 01:31:48 +1000

    Implement ControlPanel

---
+++ src/components/ControlPanel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PassButton from './PassButton.jsx';
+
+
+const ControlPanel = () => (
+    <div>
+        <PassButton />
+    </div>
+);
+
+
+export default ControlPanel;
