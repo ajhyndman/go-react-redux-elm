ab660de51683b2bfb9a15bc6993337a32fd13829
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-23 00:47:04 +1000

    Extract initial state from store declaration

--- src/store.js
+++ src/store.js
@@ -2,19 +2,22 @@ import Immutable from 'immutable';
 import { createStore } from 'redux';
 
 import C from './utils/constants';
-import reducers from './reducers';
+import reducer from './reducer';
 import range from './utils/range';
 
 
+
+const initialState = Immutable.Map({
+    turn: C.BLACK,
+    board: Immutable.List([...range(0, C.SIZE)].map(() => (
+        Immutable.List([...range(0, C.SIZE)].map(() => C.EMPTY))
+    ))),
+});
+
 // Tracks only the current board state
 const store = createStore(
-    reducers,
-    Immutable.Map({
-        turn: C.BLACK,
-        board: Immutable.List([...range(0, C.SIZE)].map(() => (
-            Immutable.List([...range(0, C.SIZE)].map(() => C.EMPTY))
-        ))),
-    })
+    reducer,
+    initialState
 );
 
 
