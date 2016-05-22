7e44f560f4d206e7f7f6363f5ef2fd9aaf07bb97
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-23 01:32:16 +1000

    Implement Container

---
+++ src/components/Container.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Board from './Board.jsx';
+import ControlPanel from './ControlPanel.jsx';
+
+
+const Container = () => (
+    <div className="container">
+        <Board />
+        <ControlPanel />
+    </div>
+);
+
+
+export default Container;
