7d55e26a5a4248befbdabbe86d299081b166d2d3
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-24 02:16:01 +1000

    Add TurnIndicator to control panel

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import PassButton from './PassButton.jsx';
+import TurnIndicator from './TurnIndicator.jsx';
 
 
 const ControlPanel = () => (
-    <div>
-        <PassButton />
+    <div className="row" style={{ textAlign: 'center' }}>
+        <div className="col s4" style={{ textAlign: 'center' }}>
+            <TurnIndicator size={36} />
+        </div>
+        <div className="col s4" style={{ textAlign: 'center' }}>
+            <PassButton />
+        </div>
     </div>
 );
 
