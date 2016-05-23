05b91a0d86a8ba7ec2fe459d011922cf15082970
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-23 22:57:35 +1000

    Use range from ImmutableJS

--- src/store.js
+++ src/store.js
@@ -3,14 +3,13 @@ import { createStore } from 'redux';
 
 import C from './utils/constants';
 import reducer from './reducer';
-import range from './utils/range';
 
 
 
 const initialState = Immutable.Map({
     turn: C.BLACK,
-    board: Immutable.List([...range(0, C.SIZE)].map(() => (
-        Immutable.List([...range(0, C.SIZE)].map(() => C.EMPTY))
+    board: Immutable.List(Immutable.Range(0, C.SIZE).map(() => (
+        Immutable.List(Immutable.Range(0, C.SIZE).map(() => C.EMPTY))
     ))),
 });
 
--- src/utils/range.js
+++
@@ -1,9 +0,0 @@
-const range = function *(start, end, step = 1) {
-    if (start < end) {
-        yield start;
-        yield *range(start + step, end, step);
-    }
-    return;
-};
-
-export default range;
