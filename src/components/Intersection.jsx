ee8e24f42b0cb5b262044fb31820c9655236c4e8
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-24 19:29:02 +1000

    Draw star points and edges differently

--- src/components/Intersection.jsx
+++ src/components/Intersection.jsx
@@ -6,65 +6,89 @@ import Stone from './Stone.jsx';
 import store from '../store';
 
 
-const ConnectedIntersection = React.createClass({
-    render: function () {
-        return (
+const ConnectedIntersection = function (props) {
+    return (
+        <div
+            style={{
+                float: 'left',
+                lineHeight: '1',
+                paddingTop: props.width + '%',
+                position: 'relative',
+                textAlign: 'center',
+                width: props.width + '%',
+            }}
+        >
             <div
+                onClick={props.onClick}
                 style={{
-                    float: 'left',
-                    lineHeight: '1',
-                    paddingTop: this.props.width + '%',
-                    position: 'relative',
-                    textAlign: 'center',
-                    width: this.props.width + '%',
+                    cursor: (props.state === C.EMPTY ? 'pointer' : 'auto'),
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
                 }}
             >
-                <div
-                    onClick={this.props.onClick}
-                    style={{
-                        cursor: (this.props.state === C.EMPTY ? 'pointer' : 'auto'),
-                        position: 'absolute',
-                        top: 0,
-                        right: 0,
-                        bottom: 0,
-                        left: 0,
-                    }}
-                >
-                    <div
+                {(props.isStarPoint
+                    ? <div
                         style={{
                             background: '#63380E',
-                            height: '1px',
-                            position: 'absolute',
-                            top: '50%',
-                            right: 0,
-                            left: 0,
-                            transform: 'translateY(-50%)',
-                        }}
-                    />
-                    <div
-                        style={{
-                            background: '#63380E',
+                            borderRadius: 9000,
+                            height: 6,
                             position: 'absolute',
                             left: '50%',
-                            top: 0,
-                            bottom: 0,
-                            transform: 'translateX(-50%)',
-                            width: '1px',
+                            top: '50%',
+                            transform: 'translate(-50%, -50%)',
+                            width: 6,
                         }}
                     />
-                    {(function () {
-                        if (this.props.state === C.BLACK) {
-                            return <Stone color="black" />;
-                        } else if (this.props.state === C.WHITE) {
-                            return <Stone color="white" />;
-                        }
-                        return null;
-                    }.bind(this))()}
-                </div>
+                    : null
+                )}
+                <div
+                    style={{
+                        background: '#63380E',
+                        height: '1px',
+                        position: 'absolute',
+                        top: '50%',
+                        right: props.isRightEdge ? '50%' : 0,
+                        left: props.isLeftEdge ? '50%' : 0,
+                        transform: 'translateY(-50%)',
+                    }}
+                />
+                <div
+                    style={{
+                        background: '#63380E',
+                        position: 'absolute',
+                        left: '50%',
+                        top: props.isTopEdge ? '50%' : 0,
+                        bottom: props.isBottomEdge ? '50%' : 0,
+                        transform: 'translateX(-50%)',
+                        width: '1px',
+                    }}
+                />
+                {(function () {
+                    if (props.state === C.BLACK) {
+                        return <Stone color="black" />;
+                    } else if (props.state === C.WHITE) {
+                        return <Stone color="white" />;
+                    }
+                    return null;
+                })()}
             </div>
-        );
-    },
-});
+        </div>
+    );
+};
+
+ConnectedIntersection.propTypes = {
+    isTopEdge: React.PropTypes.bool.isRequired,
+    isRightEdge: React.PropTypes.bool.isRequired,
+    isLeftEdge: React.PropTypes.bool.isRequired,
+    isBottomEdge: React.PropTypes.bool.isRequired,
+    isStarPoint: React.PropTypes.bool.isRequired,
+    onClick: React.PropTypes.func,
+    state: React.PropTypes.object.isRequired,
+    width: React.PropTypes.number.isRequired,
+};
 
 const Intersection = React.createClass({
     propTypes: {
