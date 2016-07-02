e525253659ec21ec4afb7b4e6753e8edb270f98d
Andrew Hyndman <ajhyndman@hotmail.com>
2016-07-03 00:49:50 +1000

    Render Intersection Component to View

--- src/View.elm
+++ src/View.elm
@@ -4,13 +4,24 @@ import Html as H
 import Html.Attributes as A
 import Html.Events as E
 
+import Constants as C
 import Model
 import Update
-import Components.Stone exposing (stone)
+import Components.Intersection exposing (intersection)
 
 
 view : Model.Model -> H.Html a
 view model =
   H.div
     []
-    [stone Model.Black 500]
+    [
+      intersection {
+        isBottomEdge = False,
+        isLeftEdge = False,
+        isRightEdge = False,
+        isTopEdge = False,
+        isStarPoint = True,
+        state = Model.Black,
+        width = C.grid_spacing
+      }
+    ]
