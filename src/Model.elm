c6695b8ebddc57f87856e83236f871ad76770db1
Andrew Hyndman <ajhyndman@hotmail.com>
2016-07-03 01:55:01 +1000

    fixup! Extract constants and init

--- src/Model.elm
+++ src/Model.elm
@@ -5,18 +5,21 @@ import Constants as C
 
 type Color = Black | White | Empty
 
+type alias Board =
+  List (List Color)
+
 type alias Model =
   {
     turn: Color,
-    board: List (List Color)
+    board: Board
   }
 
 init : Model
 init =
   {
     turn = Black,
-    board = 
-      List.map 
-        (\i -> List.map (\i -> Empty) [0..C.grid_size]) 
-        [0..C.grid_size]
+    board =
+      List.map
+        (\row -> List.map (\row -> Empty) [1..C.grid_size])
+        [1..C.grid_size]
   }
