d5993f69a4a101038e30a554a0530baa7a12877e
Andrew Hyndman <ajhyndman@hotmail.com>
2016-07-03 17:48:35 +1000

    Render Control Panel

--- src/View.elm
+++ src/View.elm
@@ -2,17 +2,30 @@ module View exposing (view)
 
 import Html as H
 import Html.Attributes as A
-import Html.Events as E
 
-import Constants as C
 import Model
 import Update
 import Components.Board exposing (board)
+import Components.ControlPanel exposing (controlPanel)
 
 
 view : Model.Model -> H.Html Update.Action
 view model =
-  H.div [A.class "container"]
+  H.div []
     [
-      board model.board
+      H.div [A.class "container"]
+        [
+          board { boardState = model.board }
+        ],
+      H.div
+        [
+          A.class "page-footer blue-grey darken-2",
+          A.style [("marginTop", "20px"), ("padding", "20px")]
+        ]
+        [
+          H.div [A.class "container"]
+            [
+              controlPanel { turn = model.turn }
+            ]
+        ]
     ]
