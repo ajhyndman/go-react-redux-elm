a5621e17738a22941e176bba2a0801dccd772817
Andrew Hyndman <ajhyndman@hotmail.com>
2017-07-23 00:24:41 +1000

    Lift materialize-css to top of project
    This dependency mutates and depends on global state.  It renders components untestable, if loaded locally.

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import 'materialize-css';
 
 import C from '../utils/constants';
 import Intersection from './Intersection';
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'materialize-css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
